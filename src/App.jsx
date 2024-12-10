56a858617b57ea24f6e8ea140f934372cca6d4ee
uguruvishwa@gmail.com <guruvishwa@GURU-VISHWA.local>
2024-12-10 19:40:10 +0530

    Boiler plate code for Campaign project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { DoubleNavbar } from './Navbar/DoubleNavbar'
+import "@mantine/core/styles.css"
+
+function App() {
+
+  return (
+    <>
+      <DoubleNavbar/>
+    </>
+  )
+}
+
+export default App
